76c008d2a25acc3b63ba2c2e4a8a506f9ea67a48
deeaenara <dayanara@aws-legalgroup.com>
2025-04-29 23:22:41 +0400

    Add files via upload

---
+++ config/swc.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  jsc: {
+    parser: {
+      syntax: 'ecmascript',
+      jsx: true
+    },
+    target: 'es2022'
+  },
+  module: {
+    type: 'es6'
+  },
+  sourceMaps: true
+};
+
